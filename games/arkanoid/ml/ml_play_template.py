d77984f52afe1d29c16cedef20774010c1da4a69
tiffany6022 <tiffany001510@gmail.com>
2020-03-18 16:03:53 +0800

    add machine play itself

--- games/arkanoid/ml/ml_play_template.py
+++ games/arkanoid/ml/ml_play_template.py
@@ -6,6 +6,7 @@ import games.arkanoid.communication as comm
 from games.arkanoid.communication import ( \
     SceneInfo, GameStatus, PlatformAction
 )
+import math
 
 def ml_loop():
     """
@@ -22,6 +23,8 @@ def ml_loop():
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here.
     ball_served = False
+    ball_down = True
+    have_end_posx = False
 
     # 2. Inform the game process that ml process is ready before start the loop.
     comm.ml_ready()
@@ -37,16 +40,51 @@ def ml_loop():
             scene_info.status == GameStatus.GAME_PASS:
             # Do some stuff if needed
             ball_served = False
+            ball_down = True
+            have_end_posx = False
 
             # 3.2.1. Inform the game process that ml process is ready
             comm.ml_ready()
             continue
 
         # 3.3. Put the code here to handle the scene information
+        if scene_info.ball[1] == 395:
+            have_end_posx = False
+        if scene_info.ball[1] >= 143 and scene_info.ball[1] < 150:
+            last_posx = scene_info.ball[0]
+        if scene_info.ball[1] >= 150 and scene_info.ball[1] < 157:
+            now_posx = scene_info.ball[0]
+            ball_down = not ball_down
+            if not ball_down:
+                continue
+            speed = now_posx - last_posx  #! if posx == 0 or 195, there is a bug.
+            move_times = math.ceil((395-scene_info.ball[1])/7)
+            if speed > 0:  # right
+                end_posx = 195 - (move_times - math.ceil((195 - now_posx) / abs(speed))) * abs(speed) # RL
+                if end_posx < 0:
+                    end_posx = ((move_times - math.ceil((195 - now_posx) / abs(speed))) - math.ceil(195 / abs(speed))) * abs(speed) # RLR
+            elif speed < 0:  # left
+                end_posx = (move_times - math.ceil(now_posx / abs(speed))) * abs(speed) # LR
+                if end_posx > 195:
+                    end_posx = 195 - ((move_times - math.ceil(now_posx / abs(speed))) - math.ceil(195 / abs(speed))) * abs(speed) #LRL
+            have_end_posx = True
+            end_posx = end_posx + 5 - end_posx % 5  # platform moves +-5
 
         # 3.4. Send the instruction for this frame to the game process
         if not ball_served:
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
+        elif have_end_posx:
+            if end_posx > scene_info.platform[0]+20:
+                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+            elif end_posx < scene_info.platform[0]+20:
+                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+            else:
+                comm.send_instruction(scene_info.frame, PlatformAction.NONE)
         else:
-            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+            if 80 > scene_info.platform[0]:
+                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+            elif 80 < scene_info.platform[0]:
+                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+            else:
+                comm.send_instruction(scene_info.frame, PlatformAction.NONE)
