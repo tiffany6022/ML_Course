25593933866fa8aa4ed96c05e4ecbc9aa789033d
tiffany6022 <47889327+tiffany6022@users.noreply.github.com>
2020-03-26 16:33:34 +0800

    Add files via upload
    add machine learning game code

--- ml_play.py
+++ ml_play.py
@@ -1,13 +1,13 @@
 """
 The template of the main script of the machine learning process
 """
-
+import pickle
+import numpy as np
 import games.arkanoid.communication as comm
 from games.arkanoid.communication import ( \
     SceneInfo, GameStatus, PlatformAction
 )
-import math
-import random
+import os.path as path
 
 
 def ml_loop():
@@ -25,103 +25,67 @@ def ml_loop():
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here.
     ball_served = False
-    ball_down = True
-    have_end_posx = False
-    position = []
-    speed_x = 0
-    speed_y = 0
+    filename = path.join(path.dirname(__file__),"save\clf_KMeans_BallAndDirection.pickle")
+    with open(filename, 'rb') as file:
+        clf = pickle.load(file)
 
     # 2. Inform the game process that ml process is ready before start the loop.
+    s = [93,93]
+    def get_direction(ball_x,ball_y,ball_pre_x,ball_pre_y):
+        VectorX = ball_x - ball_pre_x
+        VectorY = ball_y - ball_pre_y
+        if(VectorX>=0 and VectorY>=0):
+            return 0
+        elif(VectorX>0 and VectorY<0):
+            return 1
+        elif(VectorX<0 and VectorY>0):
+            return 2
+        elif(VectorX<0 and VectorY<0):
+            return 3
+	
     comm.ml_ready()
+    
 
     # 3. Start an endless loop.
     while True:
         # 3.1. Receive the scene information sent from the game process.
         scene_info = comm.get_scene_info()
-
+        feature = []
+        feature.append(scene_info.ball[0])
+        feature.append(scene_info.ball[1])
+        feature.append(scene_info.platform[0])
+        
+        feature.append(get_direction(feature[0],feature[1],s[0],s[1]))
+        s = [feature[0], feature[1]]
+        feature = np.array(feature)
+        feature = feature.reshape((-1,4))
         # 3.2. If the game is over or passed, the game process will reset
         #      the scene and wait for ml process doing resetting job.
         if scene_info.status == GameStatus.GAME_OVER or \
             scene_info.status == GameStatus.GAME_PASS:
             # Do some stuff if needed
             ball_served = False
-            ball_down = True
-            have_end_posx = False
-            position = []
-            speed_x = 0
-            speed_y = 0
 
             # 3.2.1. Inform the game process that ml process is ready
             comm.ml_ready()
             continue
 
         # 3.3. Put the code here to handle the scene information
-        if scene_info.ball[1] == 395 and not speed_y == 0:
-            have_end_posx = False
-            continue
-        position.append(scene_info.ball)
-        if len(position) == 2:
-            speed_x = position[1][0] - position[0][0]
-            speed_y = position[1][1] - position[0][1]
-            del position[0]
-        ball_down = speed_y > 0
-        if scene_info.ball[1] > random.randint(150,200) and (not have_end_posx) and abs(speed_x) >= 7:
-            if not ball_down:
-                continue
-            move_times = math.ceil((395-scene_info.ball[1])/7)
-            if speed_x > 0:    # right
-                end_posx = 195 - (move_times - math.ceil((195 - scene_info.ball[0]) / abs(speed_x))) * abs(speed_x) # RL
-                if end_posx < 0:
-                    end_posx = ((move_times - math.ceil((195 - scene_info.ball[0]) / abs(speed_x))) - math.ceil(195 / abs(speed_x))) * abs(speed_x) # RLR            
-            elif speed_x < 0:  # left
-                end_posx = (move_times - math.ceil(scene_info.ball[0] / abs(speed_x))) * abs(speed_x) # LR
-                if end_posx > 195:
-                    end_posx = 195 - ((move_times - math.ceil(scene_info.ball[0] / abs(speed_x))) - math.ceil(195 / abs(speed_x))) * abs(speed_x) #LRL
-            have_end_posx = True
-            if random.randint(0,1):
-                end_posx = end_posx + 5 - end_posx % 5  # platform moves +-5
-           
-        # old-version existing bugs 
-        '''
-        if scene_info.ball[1] >= 143 and scene_info.ball[1] < 150: #! if speed == 10, there is a bug
-            last_posx = scene_info.ball[0]
-        if scene_info.ball[1] >= 150 and scene_info.ball[1] < 157:
-            now_posx = scene_info.ball[0]
-            ball_down = not ball_down
-            if not ball_down:
-                continue
-            speed = now_posx - last_posx  #! if posx == 0 or 195, there is a bug.
-            move_times = math.ceil((395-scene_info.ball[1])/7)
-            if speed > 0:  # right
-                end_posx = 195 - (move_times - math.ceil((195 - now_posx) / abs(speed))) * abs(speed) # RL
-                if end_posx < 0:
-                    end_posx = ((move_times - math.ceil((195 - now_posx) / abs(speed))) - math.ceil(195 / abs(speed))) * abs(speed) # RLR
-            elif speed < 0:  # left
-                end_posx = (move_times - math.ceil(now_posx / abs(speed))) * abs(speed) # LR
-                if end_posx > 195:
-                    end_posx = 195 - ((move_times - math.ceil(now_posx / abs(speed))) - math.ceil(195 / abs(speed))) * abs(speed) #LRL
-            have_end_posx = True
-            # end_posx = end_posx + 5 - end_posx % 5  # platform moves +-5
-        '''
 
         # 3.4. Send the instruction for this frame to the game process
         if not ball_served:
-            if random.randint(0,1):
-                comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
-            else:
-                comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_RIGHT)
+            comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
-        elif have_end_posx:
-            if end_posx > scene_info.platform[0]+20:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-            elif end_posx < scene_info.platform[0]+20:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-            else:
-                comm.send_instruction(scene_info.frame, PlatformAction.NONE)
         else:
-            if 80 > scene_info.platform[0]:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-            elif 80 < scene_info.platform[0]:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-            else:
+                
+            y = clf.predict(feature)
+            
+            if y == 0:
                 comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+                print('NONE')
+            elif y == 1:
+                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+                print('LEFT')
+            elif y == 2:
+                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                print('RIGHT')
