64b3476db130702f4725c265fd7eebd22a502ad2
tiffany6022 <tiffany001510@gmail.com>
2020-04-15 18:19:10 +0800

    add brick position to know when to compute the position

--- ml_play.py
+++ ml_play.py
@@ -59,50 +59,48 @@ def ml_loop():
         if scene_info.ball[1] == 395 and not speed_y == 0:
             have_end_posx = False
             continue
+        brick_y = 0
+        for x in range(len(scene_info.bricks)):
+            if brick_y < scene_info.bricks[x][1]:
+                brick_y = scene_info.bricks[x][1]
+        for x in range(len(scene_info.hard_bricks)):
+            if brick_y < scene_info.hard_bricks[x][1]:
+                brick_y = scene_info.hard_bricks[x][1]
+        # print(brick_y)
         position.append(scene_info.ball)
         if len(position) == 2:
             speed_x = position[1][0] - position[0][0]
             speed_y = position[1][1] - position[0][1]
             del position[0]
         ball_down = speed_y > 0
-        if scene_info.ball[1] > random.randint(150,200) and (not have_end_posx) and abs(speed_x) >= 7:
-            if not ball_down:
+        if scene_info.ball[1] > (brick_y+30) and (not have_end_posx) and ball_down:
+            if not (abs(speed_x) == 7 or abs(speed_x) == 10):
                 continue
             move_times = math.ceil((395-scene_info.ball[1])/7)
+            # print(move_times)
             if speed_x > 0:    # right
-                end_posx = 195 - (move_times - math.ceil((195 - scene_info.ball[0]) / abs(speed_x))) * abs(speed_x) # RL
-                if end_posx < 0:
-                    end_posx = ((move_times - math.ceil((195 - scene_info.ball[0]) / abs(speed_x))) - math.ceil(195 / abs(speed_x))) * abs(speed_x) # RLR            
-            elif speed_x < 0:  # left
-                end_posx = (move_times - math.ceil(scene_info.ball[0] / abs(speed_x))) * abs(speed_x) # LR
+                end_posx = scene_info.ball[0] + move_times * abs(speed_x)
                 if end_posx > 195:
-                    end_posx = 195 - ((move_times - math.ceil(scene_info.ball[0] / abs(speed_x))) - math.ceil(195 / abs(speed_x))) * abs(speed_x) #LRL
+                    end_posx = 195 - (end_posx - 195)
+                    if end_posx < 0:
+                        end_posx = abs(end_posx)
+                        if end_posx >  195:
+                            end_posx = 195 - (end_posx - 195)
+            elif speed_x < 0:  # left
+                end_posx = scene_info.ball[0] - move_times * abs(speed_x)
+                if end_posx < 0:
+                    end_posx = abs(end_posx)
+                    if end_posx > 195:
+                        end_posx = 195 - (end_posx - 195)
+                        if end_posx <  0:
+                            end_posx = abs(end_posx)
             have_end_posx = True
+            # print(scene_info.ball, end_posx, speed_x)
+            # print(brick_y)
             if random.randint(0,1):
                 end_posx = end_posx + 5 - end_posx % 5  # platform moves +-5
-           
-        # old-version existing bugs 
-        '''
-        if scene_info.ball[1] >= 143 and scene_info.ball[1] < 150: #! if speed == 10, there is a bug
-            last_posx = scene_info.ball[0]
-        if scene_info.ball[1] >= 150 and scene_info.ball[1] < 157:
-            now_posx = scene_info.ball[0]
-            ball_down = not ball_down
-            if not ball_down:
-                continue
-            speed = now_posx - last_posx  #! if posx == 0 or 195, there is a bug.
-            move_times = math.ceil((395-scene_info.ball[1])/7)
-            if speed > 0:  # right
-                end_posx = 195 - (move_times - math.ceil((195 - now_posx) / abs(speed))) * abs(speed) # RL
-                if end_posx < 0:
-                    end_posx = ((move_times - math.ceil((195 - now_posx) / abs(speed))) - math.ceil(195 / abs(speed))) * abs(speed) # RLR
-            elif speed < 0:  # left
-                end_posx = (move_times - math.ceil(now_posx / abs(speed))) * abs(speed) # LR
-                if end_posx > 195:
-                    end_posx = 195 - ((move_times - math.ceil(now_posx / abs(speed))) - math.ceil(195 / abs(speed))) * abs(speed) #LRL
-            have_end_posx = True
-            # end_posx = end_posx + 5 - end_posx % 5  # platform moves +-5
-        '''
+            if brick_y >= 250:
+                end_posx = end_posx + 5 - end_posx % 5  # platform moves +-5
 
         # 3.4. Send the instruction for this frame to the game process
         if not ball_served:
